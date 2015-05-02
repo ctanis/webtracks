c73248c0fc019ca1d6b65a83b578406a9c04beec
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2015-05-02 14:24:00 -0400

    Add some basic styles and some more markup

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,1 +1,6 @@
 // Main JavaScript file
+$('.track-effects-toggle').on('click', function (e) {
+  var effectControls = $(this).parents('.track').find('.track-effects')
+  console.log(effectControls)
+  effectControls.toggleClass('open')
+})
